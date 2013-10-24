49e23c30cfda194457ef6baf6415c5dd693e3882
Nate Ferrero <nateferrero@gmail.com>
2013-10-24 12:12:06 +0000

    Add github link

--- web/nav-buttons.php
+++ web/nav-buttons.php
@@ -4,6 +4,8 @@
   {nav: "test",     path: "/simplified-php/test/1-variables.php", title: "Tests"}
   {nav: "tutorial", path: "/simplified-php/web/tutorial/",        title: "Tutorial"}
   {nav: "plugins",  path: "/simplified-php/web/plugins/",         title: "Plugins"}
+  {nav: "support",  path: "https://github.com/NateFerrero/simplified-php/issues?state=open"
+    title: "Bugs &amp; Support"}
 
 ]{'<a class="btn large '{nav = ? (it.nav): 'active'}'"
       href="'(it.path)'">'(it.title)'</a>'.print}
