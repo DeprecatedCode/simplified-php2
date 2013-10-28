5e2c6c9b658e23264c508aba8a0886a0a659ffe1
Nate Ferrero <nateferrero@gmail.com>
2013-10-28 07:23:13 +0000

    Better github icon

--- web/template.php
+++ web/template.php
@@ -211,7 +211,11 @@ base: {@request.path.contains ? 'simplified-php': '/simplified-php/', *: '/'}
   <script>window.hljs.initHighlightingOnLoad();</script>
 </head>
 <body>
-  <a href="https://github.com/NateFerrero/simplified-php"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
+  <a href="https://github.com/NateFerrero/simplified-php">
+    <img style="position: absolute; top: 1.333em; right: 1.333em; border: 0; height: 48px; width: 48px;"
+      src="https://github.global.ssl.fastly.net/images/modules/logos_page/GitHub-Mark.png"
+      alt="Fork me on GitHub">
+  </a>
   <h1>SimplifiedPHP: ' title '</h1>
 '.print
 
